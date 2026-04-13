0a93519e3c705d2415ac28bf785e41e21b0470ef
Fexsv <brandonstyles48@gmail.com>
2026-04-13 22:22:58 +0100

    Update route.ts

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextResponse } from 'next/server';
 import { headers } from 'next/headers';
 import { getStripe } from '@/lib/stripe';
